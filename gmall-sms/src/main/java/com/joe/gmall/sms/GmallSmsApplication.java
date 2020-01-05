d32ecdc568bd96174de576d9114668908b5f40a6
gehui <979844852@qq.com>
2020-01-05 10:41:24 +0800

    构建项目结构

---
+++ gmall-sms/src/main/java/com/joe/gmall/sms/GmallSmsApplication.java
@@ -0,0 +1,13 @@
+package com.joe.gmall.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallSmsApplication.class, args);
+	}
+
+}
