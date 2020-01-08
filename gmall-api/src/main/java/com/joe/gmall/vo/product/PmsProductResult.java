aa681143a423cd4d491231d8f54217e3d4be954f
gehui <979844852@qq.com>
2020-01-08 10:07:59 +0800

    解决添加商品时dubbo调用超时与重试3次的异常

---
+++ gmall-api/src/main/java/com/joe/gmall/vo/product/PmsProductResult.java
@@ -0,0 +1,13 @@
+package com.joe.gmall.vo.product;
+
+import lombok.Data;
+
+/**
+ * 查询单个产品进行修改时返回的结果
+ */
+@Data
+public class PmsProductResult extends PmsProductParam {
+    //商品所选分类的父id
+    private Long cateParentId;
+
+}
---
+++ gmall-api/src/main/java/com/joe/gmall/vo/product/ProductAttrInfo.java
@@ -0,0 +1,14 @@
+package com.joe.gmall.vo.product;
+
+
+import lombok.Data;
+
+/**
+ * 商品分类对应属性信息
+ */
+@Data
+public class ProductAttrInfo {
+    private Long attributeId;
+    private Long attributeCategoryId;
+
+}
