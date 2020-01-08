e62e8024f88b68e388b631e86fa36f02a03de507
gehui <979844852@qq.com>
2020-01-08 22:44:58 +0800

    销售系统数据源、日志配置整合

--- gmall-sms/src/main/java/com/joe/gmall/sms/GmallSmsApplication.java
+++ gmall-sms/src/main/java/com/joe/gmall/sms/GmallSmsApplication.java
@@ -1,8 +1,12 @@
 package com.joe.gmall.sms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDubbo
+@MapperScan("com.joe.gmall.sms.mapper")
 @SpringBootApplication
 public class GmallSmsApplication {
 
---
+++ gmall-sms/src/main/java/com/joe/gmall/sms/config/SmsDataSourceConfig.java
@@ -0,0 +1,29 @@
+package com.joe.gmall.sms.config;
+
+import io.shardingjdbc.core.api.MasterSlaveDataSourceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * @program: gmail
+ * @description
+ * @author: Joe
+ * @create: 2020-01-05 13:16
+ */
+
+@Configuration
+public class SmsDataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() throws IOException, SQLException {
+        File file = ResourceUtils.getFile("classpath:sharding-jdbc.yml");
+        DataSource dataSource = MasterSlaveDataSourceFactory.createDataSource(file);
+        return dataSource;
+    }
+}
